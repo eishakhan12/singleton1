4d949769406fb898988c0724c3dee5de11f90a8d
eishakhan12 <89078277+eishakhan12@users.noreply.github.com>
2021-08-27 12:10:25 -0700

    Initial commit

---
+++ src/com/company/SingletonDemo.java
@@ -0,0 +1,9 @@
+package com.company;
+
+// File Name: SingletonDemo.java
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Singleton tmp = Singleton.getInstance( );
+        Singleton.demoMethod( );
+    }
+}
